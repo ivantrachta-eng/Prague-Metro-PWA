e4161eda07664f51b06faca1005e369d7286b862
ivantrachta-eng <ivan.trachta@seznam.cz>
2026-08-13 06:57:36 +0200

    Refresh cache for transfer announcements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='metro-pwa-v8';
+const CACHE='metro-pwa-v9';
 const ASSETS=['./','index.html','styles.css','app.js','transit-data.json','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','audio/gonga.wav','audio/gongb.wav','audio/gongc.wav'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
