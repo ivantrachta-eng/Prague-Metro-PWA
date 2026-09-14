7173ed8e6f4548dc2de16f04ab2b618c49f8c7b0
ivantrachta-eng <ivan.trachta@seznam.cz>
2026-09-14 17:12:19 +0200

    Refresh Ostrava PWA cache for full stop names

--- ostrava/service-worker.js
+++ ostrava/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ostrava-mhd-v1';
+const CACHE='ostrava-mhd-v2';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','ostrava-transit-data.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('ostrava-mhd-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
