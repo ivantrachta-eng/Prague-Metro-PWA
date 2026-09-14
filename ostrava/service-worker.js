737d4510165db541c6aa806592ec7dfd411d71fd
ivantrachta-eng <ivan.trachta@seznam.cz>
2026-09-14 17:21:13 +0200

    Cache Ostrava playback interaction lock

--- ostrava/service-worker.js
+++ ostrava/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ostrava-mhd-v2';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','ostrava-transit-data.json'];
+const CACHE='ostrava-mhd-v3';
+const ASSETS=['./','index.html','styles.css','app.js','playback-lock.js','manifest.webmanifest','ostrava-transit-data.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('ostrava-mhd-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return resp}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./'))))});
